804d494495cf7364b23e4f93391945a5f3b60c81
ksey <ksey3000@gmail.com>
2017-11-24 09:52:09 +0300

    Update start_xd.sh

--- start_xd.sh
+++ start_xd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-XD torrents.ini &
-sleep 3
-killall XD 2> /dev/null
-sleep 3
+if [ ! -e $XD_HOME/torrents.ini ]; then
+  XD torrents.ini &
+  sleep 3
+  killall XD 2> /dev/null
+  sleep 3
+fi
 sed -i -e 's/bind.*=.*[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]:[[:digit:]]\+/bind='"$XD_IP:$XD_PORT"'/g' torrents.ini
 sed -i -e 's/address.*=.*[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]:[[:digit:]]\+/address='"$I2P_ROUTER"'/g' torrents.ini
 sed -i -e 's#\ *=\ *storage#='"$XD_HOME/storage"'#g' torrents.ini
