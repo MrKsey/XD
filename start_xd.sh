04497ca508de042fe53a4222b42ca654cf15628e
ksey <ksey3000@gmail.com>
2017-12-25 20:22:08 +0300

    Update start_xd.sh

--- start_xd.sh
+++ start_xd.sh
@@ -5,13 +5,13 @@ if [ $HOST_USER_ID -le 499 ] || [ $HOST_GROUP_ID -eq 0 ]; then
   exit
 fi
 
+chmod -R 777 "$XD_HOME"
 addgroup -g $HOST_GROUP_ID xd 2> /dev/null
 groupmod -g $HOST_GROUP_ID $(cat /etc/group | grep xd: | cut -d: -f1) 2> /dev/null
 adduser -D -s /bin/sh -u $HOST_USER_ID -G $(cat /etc/group | grep x:$HOST_GROUP_ID: | cut -d: -f1) -h "$XD_HOME" xd 2> /dev/null
 usermod -o -u $HOST_USER_ID -g $(cat /etc/group | grep x:$HOST_GROUP_ID: | cut -d: -f1) -d "$XD_HOME" -m xd 2> /dev/null
 addgroup xd $(cat /etc/group | grep x:$HOST_GROUP_ID: | cut -d: -f1) 2> /dev/null
 chown -R xd:$(cat /etc/group | grep x:$HOST_GROUP_ID: | cut -d: -f1) "$XD_HOME"
-chmod -R 777 "$XD_HOME"
 su - xd -c "echo 'umask 0000'>~/.profile"
 
 if [ ! -e $XD_HOME/torrents.ini ]; then
